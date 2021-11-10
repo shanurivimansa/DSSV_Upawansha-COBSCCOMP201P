0cdf3e8646da78d1a33390b0756ece1305b5067f
Sathsara Maduranga <sathsara.emedia@gmail.com>
2021-11-10 22:02:36 +0530

    Added new view controllers and view controller classes,Tab bar configured

---
+++ NIBM_Parking/VC + VM/Main/BookingVC.swift
@@ -0,0 +1,20 @@
+//
+//  BookingVC.swift
+//  NIBM_Parking
+//
+//  Created by Shanuri Vimansa on 2021-11-10.
+//
+
+import UIKit
+
+class BookingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ NIBM_Parking/VC + VM/Main/ProfileVC.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileVC.swift
+//  NIBM_Parking
+//
+//  Created by Shanuri Vimansa on 2021-11-10.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
