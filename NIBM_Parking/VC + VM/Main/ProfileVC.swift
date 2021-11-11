01e57db5034e73dc671dd2504b638aa5bdf3e9fa
Sathsara Maduranga <sathsara.emedia@gmail.com>
2021-11-12 00:32:27 +0530

    log out current user

--- NIBM_Parking/VC + VM/Main/ProfileVC.swift
+++ NIBM_Parking/VC + VM/Main/ProfileVC.swift
@@ -8,14 +8,24 @@
 import UIKit
 
 class ProfileVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
-
-
+    
+    @IBAction func logOutUser(_ sender: Any) {
+        
+        
+        
+        // Delete Realm current user
+        guard let user = LocalUser.current() else { return }
+        RealmService.shared.delete(user)
+        self.sceneDelegate?.navigateToRegardingView()
+        
+    }
+    
+    
 }
